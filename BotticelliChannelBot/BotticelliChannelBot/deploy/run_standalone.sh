2e08d4ee5689dac398c2d41289299bff1decb780
devgopher <igevdo@mail.ru>
2023-09-27 17:09:33 +0300

    - deployment script

--- BotticelliChannelBot/BotticelliChannelBot/deploy/run_standalone.sh
+++ BotticelliChannelBot/BotticelliChannelBot/deploy/run_standalone.sh
@@ -1,14 +1,15 @@
 sudo apt-get update && \  sudo apt-get install -y dotnet-sdk-7.0
 
-rm -rf botticelli/
-git clone https://github.com/devgopher/botticelli.git
-pushd botticelli/
-git checkout mvp/0.1
+rm -rf botticelli_channel_bot
+git clone https://github.com/devgopher/botticelli_channel_bot.git
+pushd botticelli_channel_bot
+git checkout develop
 git pull
+git submodule update --init --remote --recursive
 
-pushd TelegramMessagingSample
+pushd BotticelliChannelBot/BotticelliChannelBot
 
-dotnet run TelegramMessagingSample.csproj &
+nohup dotnet run &
 
 echo BOT ID:
 cat Data/botId
